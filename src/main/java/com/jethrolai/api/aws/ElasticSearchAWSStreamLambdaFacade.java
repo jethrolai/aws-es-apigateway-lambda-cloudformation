7390aa813d473809a14a91417479425664efd942
Jethro Lai <jethro@jethrolai.com>
2017-08-09 07:33:40 -0700

    Update javadoc and remove unused method

--- src/main/java/com/jethrolai/api/aws/ElasticSearchAWSStreamLambdaFacade.java
+++ src/main/java/com/jethrolai/api/aws/ElasticSearchAWSStreamLambdaFacade.java
@@ -25,141 +25,133 @@ import io.searchbox.core.Search;
 
 /**
  * 
- * This is an implementation of AWS Lambda function that serves as a facade api between AWS ElasticSearch service's restful api
- * and AWS Api Gateway.
+ * This is an implementation of AWS Lambda function that serves as a facade api
+ * between AWS ElasticSearch service's restful api and AWS Api Gateway.
  * 
  * @author jlai
  *
  */
 public class ElasticSearchAWSStreamLambdaFacade implements RequestStreamHandler {
-    /**
-     * This must be set through AWS Lambda
-     */
-    final static private String ES_HOST_ENV_VAR_KEY = "ES_HOST";
-    final static private String QUERY_PARAMETERS = "queryStringParameters";
-    final static private String DEFAULT_INDEX = "index";
-    final static private String DEFAULT_TYPE = "type";
+	/**
+	 * This must be set through AWS Lambda
+	 */
+	final static private String ES_HOST_ENV_VAR_KEY = "ES_HOST";
+	final static private String QUERY_PARAMETERS = "queryStringParameters";
+	final static private String DEFAULT_INDEX = "index";
+	final static private String DEFAULT_TYPE = "type";
 
-    private JSONParser parser = new JSONParser();
+	private JSONParser parser = new JSONParser();
 
-    /**
-     * entry point of lambda function
-     */
-    
-    @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-    	// TODO validation: check if required environmental variables are provided; if not, log the error, issue
-        // alert/notification and gracefully quit.
-        LambdaLogger logger = this.setUpLogger(context);
-        logger.log("Initializing ElasticSearch Lambda Facade ...");
+	/**
+	 * entry point of lambda function. This assumes API Gateway uses Lambda
+	 * Proxy Integration where all external requests are proxied to lambda with
+	 * details in the "event" object of input context.
+	 */
 
-        logger.log("Checking required environment setup ...");
-        String esHost = System.getenv(ES_HOST_ENV_VAR_KEY);
-        if (esHost == null)
-            throw new RuntimeException(String.format("Environmental variable \"%s\" must be provided.", ES_HOST_ENV_VAR_KEY));
-        else if (esHost.startsWith("http://") == false && esHost.startsWith("https://") == false)
-            esHost = "http://" + esHost;
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+		// TODO validation: check if required environmental variables are
+		// provided; if not, log the error, issue
+		// alert/notification and gracefully quit.
+		LambdaLogger logger = this.setUpLogger(context);
+		logger.log("Initializing ElasticSearch Lambda Facade ...");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        JSONObject responseJson = new JSONObject();
-        BoolQueryBuilder queryBuilder = new BoolQueryBuilder();
-        logger.log("Function initialized.");
+		logger.log("Checking required environment setup ...");
+		String esHost = System.getenv(ES_HOST_ENV_VAR_KEY);
+		if (esHost == null)
+			throw new RuntimeException(
+					String.format("Environmental variable \"%s\" must be provided.", ES_HOST_ENV_VAR_KEY));
+		else if (esHost.startsWith("http://") == false && esHost.startsWith("https://") == false)
+			esHost = "http://" + esHost;
 
-        // Parse incoming request and set up search term
-        try {
-            JSONObject event = (JSONObject) parser.parse(reader);
-            logger.log("Event:\n\n");
-            logger.log(((JSONObject) event).toString());
-            if (event.get(QUERY_PARAMETERS) != null) {
-                JSONObject queryParameters = (JSONObject) event.get(QUERY_PARAMETERS);
-                logger.log(String.format("Setting query terms ..."));
-                queryParameters.forEach((k, v) -> {
-                    queryBuilder.must(QueryBuilders.matchQuery((String) k, v));
-                    logger.log(String.format("  query term: \"%s:%s\" added.", k, v));
-                });
-                logger.log(String.format("Setting query terms completed!"));
-            } else {
-                logger.log("No query parameter provided.");
-            }
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		JSONObject responseJson = new JSONObject();
+		BoolQueryBuilder queryBuilder = new BoolQueryBuilder();
+		logger.log("Function initialized.");
 
-            logger.log(String.format("Search will be performed on index:%s and type:%s", this.getIndex(event), this.getType(event)));
-            Search search = (Search) new Search.Builder(String.format("{\"query\":%s}", queryBuilder.toString()))
-                    // TODO multiple indexs and/or types can be supported
-                    .addIndex(this.getIndex(event)).addType(this.getType(event)).build();
+		// Parse incoming request and set up search term
+		try {
+			JSONObject event = (JSONObject) parser.parse(reader);
+			logger.log("Event:\n\n");
+			logger.log(((JSONObject) event).toString());
+			if (event.get(QUERY_PARAMETERS) != null) {
+				JSONObject queryParameters = (JSONObject) event.get(QUERY_PARAMETERS);
+				logger.log(String.format("Setting query terms ..."));
+				queryParameters.forEach((k, v) -> {
+					queryBuilder.must(QueryBuilders.matchQuery((String) k, v));
+					logger.log(String.format("  query term: \"%s:%s\" added.", k, v));
+				});
+				logger.log(String.format("Setting query terms completed!"));
+			} else {
+				logger.log("No query parameter provided.");
+			}
 
-            // Initialize JsetClient as search utility
-            JestClientFactory factory = new JestClientFactory();
-            HttpClientConfig clientConfig = new HttpClientConfig.Builder(esHost).multiThreaded(true).defaultMaxTotalConnectionPerRoute(10)
-                    .maxTotalConnection(20).build();
-            factory.setHttpClientConfig(clientConfig);
-            JestClient client = factory.getObject();
+			logger.log(String.format("Search will be performed on index:%s and type:%s", this.getIndex(event),
+					this.getType(event)));
+			Search search = (Search) new Search.Builder(String.format("{\"query\":%s}", queryBuilder.toString()))
+					// TODO multiple indexs and/or types can be supported
+					.addIndex(this.getIndex(event)).addType(this.getType(event)).build();
 
-            logger.log("Performing searching ...");
-            JestResult result = client.execute(search);
-            logger.log("Completed.");
+			// Initialize JsetClient as search utility
+			JestClientFactory factory = new JestClientFactory();
+			HttpClientConfig clientConfig = new HttpClientConfig.Builder(esHost).multiThreaded(true)
+					.defaultMaxTotalConnectionPerRoute(10).maxTotalConnection(20).build();
+			factory.setHttpClientConfig(clientConfig);
+			JestClient client = factory.getObject();
 
-            logger.log("Constructing response ...");
-            JSONObject headerJson = new JSONObject();
-            headerJson.put("Content-Type", "application/json");
-            responseJson.put("statusCode", "200");
-            responseJson.put("headers", headerJson);
-            responseJson.put("body", result.getJsonString());
-            responseJson.put("isBase64Encoded", "true");
-            logger.log("Completed.");
-        }
-        catch (ParseException pex) {
-            logger.log("Failed to parse incoming request ...");
-            responseJson.put("statusCode", "400");
-            responseJson.put("exception", pex);
-            logger.log(pex.getMessage());
-        }
+			logger.log("Performing searching ...");
+			JestResult result = client.execute(search);
+			logger.log("Completed.");
 
-        logger.log("Writing response to output stream ...");
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-        writer.write(responseJson.toJSONString());
-        writer.close();
-        logger.log("Completed.");
-    }
+			logger.log("Constructing response ...");
+			JSONObject headerJson = new JSONObject();
+			headerJson.put("Content-Type", "application/json");
+			responseJson.put("statusCode", "200");
+			responseJson.put("headers", headerJson);
+			responseJson.put("body", result.getJsonString());
+			responseJson.put("isBase64Encoded", "true");
+			logger.log("Completed.");
+		} catch (ParseException pex) {
+			logger.log("Failed to parse incoming request ...");
+			responseJson.put("statusCode", "400");
+			responseJson.put("exception", pex);
+			logger.log(pex.getMessage());
+		}
 
-    
-    /**
-     * This method is only to gain testability
-     * @param inputStream
-     * @param outputStream
-     * @param context
-     * @throws IOException
-     */
-    @SuppressWarnings("unchecked")
-    protected void handle(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        
-    }
-    
-    /**
-     * Dummy encapsulation for unit test 
-     * @param context
-     * @return
-     */
-    protected LambdaLogger setUpLogger(Context context){
-        return context.getLogger();
-    }
+		logger.log("Writing response to output stream ...");
+		OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+		writer.write(responseJson.toJSONString());
+		writer.close();
+		logger.log("Completed.");
+	}
 
-    /**
-     * TODO implement query for specified index
-     * 
-     * @param event
-     *            parsed request as JSONOject
-     */
-    private String getIndex(JSONObject event) {
-        return DEFAULT_INDEX;
-    }
+	/**
+	 * Extra encapsulation for testability
+	 * 
+	 * @param context
+	 * @return
+	 */
+	protected LambdaLogger setUpLogger(Context context) {
+		return context.getLogger();
+	}
 
-    /**
-     * TODO implement query for specified type
-     * 
-     * @param event
-     *            parsed request as JSONOject
-     */
-    private String getType(JSONObject event) {
-        return DEFAULT_TYPE;
-    }
+	/**
+	 * TODO implement query for specified index
+	 * 
+	 * @param event
+	 *            parsed request as JSONOject
+	 */
+	private String getIndex(JSONObject event) {
+		return DEFAULT_INDEX;
+	}
+
+	/**
+	 * TODO implement query for specified type
+	 * 
+	 * @param event
+	 *            parsed request as JSONOject
+	 */
+	private String getType(JSONObject event) {
+		return DEFAULT_TYPE;
+	}
 }
